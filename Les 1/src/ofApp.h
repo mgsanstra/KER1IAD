4e1449ede5830607fe1b73c7b6450010275237fd
Michael Sanstra <michaelsanstra@gmail.com>
2017-09-12 13:21:46 +0200

    Fixed crashing issue

--- Les 1/src/ofApp.h
+++ Les 1/src/ofApp.h
@@ -3,7 +3,6 @@
 #include "ofMain.h"
 #include "ofApp.h"
 #include "Ball.h"
-#incude "ofxGui.h"
 
 #define NBALLS 5
 
@@ -19,24 +18,7 @@ public:
 	void draw();
 	
 	void keyPressed(int key);
-	void keyReleased(int key);
-	void mouseMoved(int x, int y);
-	void mouseDragged(int x, int y, int button);
-	void mousePressed(int x, int y, int button);
-	void mouseReleased(int x, int y, int button);
-	void mouseEntered(int x, int y);
-	void mouseExited(int x, int y);
-	void windowResized(int w, int h);
-	void dragEvent(ofDragInfo dragInfo);
-	void gotMessage(ofMessage msg);
-	
-	ofxPanel gui;
-	ofParameter<int> radius;
-	ofParameter<float> xSpeed;
-	ofParameter<float> ySpeed;
-	ofParameter<ofColor> color;
-	
-	
+
 private:
 	float x;
 	float y;
